c7bfb513f5fd738d2fd928594bf916b222c97cac
Leonardo luis rampazi <105321172+Leonardo-Rampazi@users.noreply.github.com>
2022-07-26 19:19:22 -0300

    colocando blog pessoal com react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css';
+function home(){
+    return(
+        <>
+        <h1 className='titulo'>Home</h1>
+       <img src="https://pt.psg.fr/media/2874/psg_logo_seul_volume_rvb.png" alt="imagem inicial" className='img'/>
+        </>
+    );
+}
+export default home;
